a5f0a1ffa65e638e0cc9338c06f6b5769be7fe6a
PaulGent84 <paul.gent@bigbite.net>
2024-03-14 09:58:20 +0000

    Chore: adds ignore for lint errors

--- private/src/scripts/modules/paragraph-width.js
+++ private/src/scripts/modules/paragraph-width.js
@@ -3,12 +3,14 @@ const restrictParagraphWidth = () => {
   const paragraphs = document.querySelectorAll('p');
   // For each paragraph, check if its width is greater than 1000px
   paragraphs.forEach((p) => {
+    /* eslint-disable no-param-reassign */
     if (p.clientWidth > 1000) {
       // If so, restrict its width to 860px
       p.style.maxWidth = '860px';
       // Center the paragraph
       p.style.marginLeft = 'auto';
       p.style.marginRight = 'auto';
+      /* eslint-enable no-param-reassign */
     }
   });
 };
