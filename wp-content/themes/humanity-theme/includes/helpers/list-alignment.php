921f5e51659830dfafff22a75690a5eb202a38a6
PaulGent84 <paul.gent@bigbite.net>
2024-03-15 11:02:28 +0000

    Chore: fix more syntax errors

--- wp-content/themes/humanity-theme/includes/helpers/list-alignment.php
+++ wp-content/themes/humanity-theme/includes/helpers/list-alignment.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( ! function_exists( 'Add_Alignment_Controls_To_List_block' ) ) {
+if ( ! function_exists( 'add_alignment_controls_to_list_block' ) ) {
 	/**
 	 * * Add alignment support to the core/list block
 	 *
@@ -8,12 +8,12 @@ if ( ! function_exists( 'Add_Alignment_Controls_To_List_block' ) ) {
 	 *
 	 * @return array $metadata - The modified block type metadata
 	 */
-	function Add_Alignment_Controls_To_List_block( $metadata ) {
-		if ('core/list' === $metadata['name'] ) {
+	function add_alignment_controls_to_list_block( $metadata ) {
+		if ( 'core/list' === $metadata['name'] ) {
 			$metadata['supports']['align'] = true;
 		}
 		return $metadata;
 	}
 }
 
-add_filter('block_type_metadata', 'Add_Alignment_Controls_To_List_block');
+add_filter( 'block_type_metadata', 'add_alignment_controls_to_list_block' );
