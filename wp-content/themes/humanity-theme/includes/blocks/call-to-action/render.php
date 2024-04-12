271822a1d2b230026afd1244d1cfa8dce9a4339e
PaulGent84 <paul.gent@bigbite.net>
2024-04-12 11:27:26 +0100

    removes unnecessary function and uses block content for inner blocks

--- wp-content/themes/humanity-theme/includes/blocks/call-to-action/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/call-to-action/render.php
@@ -2,25 +2,6 @@
 
 declare( strict_types = 1 );
 
-if ( ! function_exists( 'render_cta_inner_blocks' ) ) {
-	/**
-	 * Render inner blocks
-	 *
-	 * @package Amnesty\Blocks
-	 *
-	 * @param array $inner_blocks the inner blocks
-	 *
-	 * @return string
-	 */
-	function render_cta_inner_blocks( $inner_blocks = [] ) {
-		if ( ! empty( $inner_blocks ) ) {
-			foreach ( $inner_blocks as $inner_block ) {
-				return render_block( $inner_block );
-			}
-		}
-	}
-}
-
 if ( ! function_exists( 'amnesty_render_cta_block' ) ) {
 	/**
 	 * Render a download block
@@ -31,31 +12,14 @@ if ( ! function_exists( 'amnesty_render_cta_block' ) ) {
 	 *
 	 * @return string|null
 	 */
-	function amnesty_render_cta_block( $attrs = [] ) {
-		// Get the post content
-		global $post;
-
-		// Get the blocks
-		$blocks = parse_blocks( $post->post_content );
-
-		$inner_blocks = '';
-
-		// Loop through the blocks
-		foreach ( $blocks as $block ) {
-			// Check if the block is the call to action block
-			if ( 'amnesty-core/block-call-to-action' === $block['blockName'] ) {
-				// Get the inner blocks
-				$inner_blocks = $block['innerBlocks'];
-			}
-		}
-
+	function amnesty_render_cta_block( $attrs, $content) {
 		// Apply filters to the attributes
 		$attrs = apply_filters( 'amnesty_cta_block_attributes', $attrs );
 
 		// Parse the attributes
 		$pre_heading = isset( $attrs['preheading'] ) ? $attrs['preheading'] : '';
 		$heading     = isset( $attrs['title'] ) ? $attrs['title'] : '';
-		$content     = isset( $attrs['content'] ) ? $attrs['content'] : '';
+		$cta_content     = isset( $attrs['content'] ) ? $attrs['content'] : '';
 
 		// Set the classes
 		$classes = classnames(
@@ -77,8 +41,8 @@ if ( ! function_exists( 'amnesty_render_cta_block' ) ) {
 			esc_attr( $classes ),
 			wp_kses_post( $pre_heading ),
 			wp_kses_post( $heading ),
-			wp_kses_post( $content ),
-			render_cta_inner_blocks( $inner_blocks ),
+			wp_kses_post( $cta_content ),
+			wp_kses_post( $content )
 		);
 	}
 }
