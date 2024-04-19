e3358128d90d63a72767c4a39bdbd387c96ec9c6
PaulGent84 <paul.gent@bigbite.net>
2024-04-19 12:32:07 +0100

    registers and renders the background media block in php for fse

---
+++ private/src/scripts/editor/blocks/background-media/deprecated.jsx
@@ -0,0 +1,23 @@
+import classnames from 'classnames';
+
+const v1 = {
+  save({ className, innerBlocks }) {
+    const leftImage = innerBlocks[0]?.attributes?.image?.id;
+    const rightImage = innerBlocks[1]?.attributes?.image?.id;
+
+    const blockClasses = classnames(className, {
+      'has-imageLeft': !!leftImage,
+      'has-imageRight': !!rightImage,
+    });
+
+    return (
+      <div className={blockClasses}>
+        <InnerBlocks.Content />
+      </div>
+    );
+  },
+};
+
+const deprecated = [v1];
+
+export default deprecated;
