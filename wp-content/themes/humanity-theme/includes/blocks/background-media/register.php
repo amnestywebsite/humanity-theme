ea9e3f35ad4eb13806ef0288d5ddf3e646cdbead
PaulGent84 <paul.gent@bigbite.net>
2024-04-22 11:36:06 +0100

    fixes build errors

--- wp-content/themes/humanity-theme/includes/blocks/background-media/register.php
+++ wp-content/themes/humanity-theme/includes/blocks/background-media/register.php
@@ -2,7 +2,14 @@
 
 declare( strict_types = 1 );
 
-if ( !function_exists('register_background_media_block') ) {
+if ( ! function_exists( 'register_background_media_block' ) ) {
+	/**
+	 * Register the Background Media block
+	 *
+	 * @package Amnesty\Blocks
+	 *
+	 * @return void
+	 */
 	function register_background_media_block(): void {
 		register_block_type(
 			'amnesty-core/background-media',
--- wp-content/themes/humanity-theme/includes/blocks/background-media/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/background-media/render.php
@@ -2,7 +2,17 @@
 
 declare( strict_types = 1 );
 
-if ( !function_exists( 'render_background_media_block' ) ) {
+if ( ! function_exists( 'render_background_media_block' ) ) {
+	/**
+	 * Render the Background Media block
+	 *
+	 * @package Amnesty\Blocks
+	 *
+	 * @param array<string,mixed> $attributes the block attributes
+	 * @param string              $content the block content
+	 *
+	 * @return string
+	 */
 	function render_background_media_block( array $attributes, $content ) {
 		return sprintf(
 			'<div %1$s>%2$s</div>',
