cad15b3ddf059c3a1f72f2fb6e4527d2a5664052
PaulGent84 <paul.gent@bigbite.net>
2024-04-22 11:33:33 +0100

    fixes the font family variables for FSE

--- wp-content/themes/humanity-theme/includes/blocks/background-media/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/background-media/render.php
@@ -4,9 +4,6 @@ declare( strict_types = 1 );
 
 if ( !function_exists( 'render_background_media_block' ) ) {
 	function render_background_media_block( array $attributes, $content ) {
-		echo '<pre>';
-		var_dump( $content );
-		echo '</pre>';
 		return sprintf(
 			'<div %1$s>%2$s</div>',
 			wp_kses_data( get_block_wrapper_attributes() ),
