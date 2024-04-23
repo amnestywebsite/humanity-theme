5048d6b29d2e038e6013e07f1c7726bc238ba612
PaulGent84 <paul.gent@bigbite.net>
2024-04-23 11:56:18 +0100

    removed unnecessary code since the block uses inner blocks the block was being rendered twice

--- wp-content/themes/humanity-theme/includes/blocks/call-to-action/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/call-to-action/render.php
@@ -8,41 +8,12 @@ if ( ! function_exists( 'amnesty_render_cta_block' ) ) {
 	 *
 	 * @package Amnesty\Blocks
 	 *
-	 * @param array $attrs the block attributes
+	 * @param array  $attrs      the block attributes
+	 * @param string $content    the block content
 	 *
-	 * @return string|null
+	 * @return string
 	 */
-	function amnesty_render_cta_block( $attrs, $content) {
-		// Apply filters to the attributes
-		$attrs = apply_filters( 'amnesty_cta_block_attributes', $attrs );
-
-		// Parse the attributes
-		$pre_heading = isset( $attrs['preheading'] ) ? $attrs['preheading'] : '';
-		$heading     = isset( $attrs['title'] ) ? $attrs['title'] : '';
-		$cta_content = isset( $attrs['content'] ) ? $attrs['content'] : '';
-
-		// Set the classes
-		$classes = classnames(
-			'callToAction',
-			[
-				"callToAction--{$attrs['background']}" => (bool) $attrs['background'],
-			]
-		);
-
-		return sprintf(
-			'<div class="%1$s" role="note" aria-label="%3$s">
-				<h2 class="callToAction-preHeading">%2$s</h2>
-				<h1 class="callToAction-heading">%3$s</h1>
-				<p class="callToAction-content">%4$s</p>
-        		<div className="innerBlocksContainer">
-					%5$s
-        		</div>
-      		</div>',
-			esc_attr( $classes ),
-			wp_kses_post( $pre_heading ),
-			wp_kses_post( $heading ),
-			wp_kses_post( $cta_content ),
-			wp_kses_post( $content )
-		);
+	function amnesty_render_cta_block( array $attrs, string $content = '' ): string {
+		return wp_kses_post( $content );
 	}
 }
