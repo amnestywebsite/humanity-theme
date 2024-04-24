95a8083d432cafb8a978cc502392de1588ea5d62
Jay McPartland <jay@bigbite.net>
2024-04-24 16:19:15 +0100

    Reorder block registration

--- wp-content/themes/humanity-theme/includes/blocks/register.php
+++ wp-content/themes/humanity-theme/includes/blocks/register.php
@@ -28,8 +28,10 @@ require_once __DIR__ . '/embed-sutori/register.php';
 require_once __DIR__ . '/embed-sutori/render.php';
 require_once __DIR__ . '/embed-tickcounter/register.php';
 require_once __DIR__ . '/embed-tickcounter/render.php';
-require_once __DIR__ . '/header/register.php';
 require_once __DIR__ . '/header/class-header-block-renderer.php';
+require_once __DIR__ . '/header/register.php';
+require_once __DIR__ . '/hero/register.php';
+require_once __DIR__ . '/hero/render.php';
 require_once __DIR__ . '/iframe-button/register.php';
 require_once __DIR__ . '/iframe-button/render.php';
 require_once __DIR__ . '/iframe/register.php';
@@ -38,14 +40,16 @@ require_once __DIR__ . '/image/register.php';
 require_once __DIR__ . '/image/render.php';
 require_once __DIR__ . '/link-group/register.php';
 require_once __DIR__ . '/link-group/render.php';
-require_once __DIR__ . '/post-list/register.php';
-require_once __DIR__ . '/post-list/render.php';
 require_once __DIR__ . '/menu/register.php';
 require_once __DIR__ . '/menu/render.php';
 require_once __DIR__ . '/petition-list/register.php';
 require_once __DIR__ . '/petition-list/render.php';
+require_once __DIR__ . '/post-list/register.php';
+require_once __DIR__ . '/post-list/render.php';
 require_once __DIR__ . '/regions/register.php';
 require_once __DIR__ . '/regions/render.php';
+require_once __DIR__ . '/related-content/register.php';
+require_once __DIR__ . '/related-content/render.php';
 require_once __DIR__ . '/section/class-section-block-renderer.php';
 require_once __DIR__ . '/section/register.php';
 require_once __DIR__ . '/slider/register.php';
@@ -54,10 +58,6 @@ require_once __DIR__ . '/term-list/register.php';
 require_once __DIR__ . '/term-list/render.php';
 require_once __DIR__ . '/tweet-action/register.php';
 require_once __DIR__ . '/tweet-action/render.php';
-require_once __DIR__ . '/related-content/register.php';
-require_once __DIR__ . '/related-content/render.php';
-require_once __DIR__ . '/hero/register.php';
-require_once __DIR__ . '/hero/render.php';
 
 if ( ! function_exists( 'amnesty_register_php_rendered_blocks' ) ) {
 	/**
@@ -75,24 +75,24 @@ if ( ! function_exists( 'amnesty_register_php_rendered_blocks' ) ) {
 		register_custom_card_block();
 		register_download_block();
 		register_flourish_embed_block();
-		register_infogram_embed_block();
-		register_sutori_embed_block();
-		register_tickcounter_embed_block();
 		register_header_block();
-		register_iframe_button_block();
+		register_hero_block();
 		register_iframe_block();
+		register_iframe_button_block();
 		register_image_block();
+		register_infogram_embed_block();
 		register_link_group_block();
 		register_list_block();
 		register_menu_block();
 		register_petition_list_block();
 		register_regions_block();
+		register_related_content_block();
 		register_section_block();
 		register_slider_block();
+		register_sutori_embed_block();
 		register_term_list_block();
+		register_tickcounter_embed_block();
 		register_tweet_action_block();
-		register_related_content_block();
-		register_hero_block();
 	}
 }
 
