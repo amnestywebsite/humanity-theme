3b70129faf080644b6b4b36d753ea7548f553dc4
PaulGent84 <paul.gent@bigbite.net>
2024-04-30 12:02:12 +0100

    removes render mark up that was causing a double wrapper container

--- wp-content/themes/humanity-theme/includes/blocks/links-with-icons/render.php
+++ wp-content/themes/humanity-theme/includes/blocks/links-with-icons/render.php
@@ -14,44 +14,8 @@ if ( ! function_exists( 'render_links_with_icons_block' ) ) {
 	 * @return string
 	 */
 	function render_links_with_icons_block( array $attrs = [], string $content = '' ): string {
-		$attrs = wp_parse_args(
-			$attrs,
-			[
-				'quantity'        => 0,
-				'orientation'     => 'horizontal',
-				'backgroundColor' => '',
-				'hideLines'       => false,
-				'dividerIcon'     => 'none',
-				'className'       => '',
-			]
-		);
-
-		$quantity         = $attrs['quantity'];
-		$orientation      = $attrs['orientation'];
-		$background_color = $attrs['backgroundColor'];
-		$hide_lines       = $attrs['hideLines'];
-		$divider_icon     = $attrs['dividerIcon'];
-		$is_style_square  = $attrs['className'];
-
-
-		$classes = classnames(
-			'linksWithIcons-group',
-			[
-				'is-' . $orientation,
-				'has-' . $quantity . '-items',
-				'has-background'        => (bool) $background_color,
-				'has-no-lines'          => (bool) $hide_lines,
-				'has-' . $background_color . '-background-color' => (bool) $background_color,
-				'icon-' . $divider_icon => (bool) $divider_icon,
-				$is_style_square,
-			]
-		);
-
 		return sprintf(
-			'<div class="%1$s">
-				%2$s
-			</div>',
-			esc_attr( $classes ),
+			'%1$s',
 			wp_kses_post( $content )
 		);
 	}
