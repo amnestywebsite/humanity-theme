a4b57c6886b19c7d5a1eb4340ed788d57836d988
PaulGent84 <paul.gent@bigbite.net>
2024-05-09 11:02:50 +0100

    adds 66/33 text with image block pattern

--- wp-content/themes/humanity-theme/includes/block-patterns/social-share.php
+++ wp-content/themes/humanity-theme/includes/block-patterns/social-share.php
@@ -27,6 +27,18 @@ if ( ! function_exists( 'amnesty_register_block_patterns' ) ) {
 				'content'     => file_get_contents( __DIR__ . '/views/social-share.html' ),
 			]
 		);
+
+		register_block_pattern(
+			'amnesty/66-33-text-with-image',
+			[
+				/* translators: [admin] */
+				'title'       => __( '66/33 Text with Image', 'amnesty' ),
+				/* translators: [admin] */
+				'description' => __( 'Two column layout with text and image', 'amnesty' ),
+				'categories'  => [ 'amnesty-core' ],
+				'content'     => file_get_contents( __DIR__ . '/views/66-33-text-with-image.html' ),
+			]
+		);
 	}
 }
 
