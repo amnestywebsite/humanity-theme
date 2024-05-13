93ecaf620bbd10de2a7178cdc8a1d44f65485aa8
PaulGent84 <paul.gent@bigbite.net>
2024-05-13 15:14:43 +0100

    Register and build pattern in single php file

--- wp-content/themes/humanity-theme/patterns/chunky-heading-with-links.php
+++ wp-content/themes/humanity-theme/patterns/chunky-heading-with-links.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * Title: Chunky Heading with Links
+ * Description: A chunky heading with links to page points
+ * Slug: amnesty/chunky-heading-with-links
+ * Keywords: heading, links
+ * Categories: humanity
+ */
+?>
+
 <!-- wp:amnesty-core/block-section {"background":"grey"} -->
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
 <h2 class="wp-block-heading has-text-align-center has-x-large-font-size">Chapter 1 – Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
