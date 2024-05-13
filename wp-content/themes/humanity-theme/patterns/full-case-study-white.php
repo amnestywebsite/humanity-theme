8279a590f171bf9e1ef826ebd3ed3432d2ef52f7
PaulGent84 <paul.gent@bigbite.net>
2024-05-13 16:23:46 +0100

    Fixes syntax errors

--- wp-content/themes/humanity-theme/patterns/full-case-study-white.php
+++ wp-content/themes/humanity-theme/patterns/full-case-study-white.php
@@ -10,27 +10,27 @@
 <!-- wp:amnesty-core/block-section {"background":"","sectionId":"casestudy","sectionName":"Case Study"} -->
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-    <div class="wp-block-column"><!-- wp:image -->
-    <figure class="wp-block-image"><img alt=""/></figure>
-    <!-- /wp:image --></div>
-    <!-- /wp:column -->
+	<div class="wp-block-column"><!-- wp:image -->
+	<figure class="wp-block-image"><img alt=""/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:column -->
 
-    <!-- wp:column -->
-    <div class="wp-block-column"><!-- wp:heading -->
-    <h2 class="wp-block-heading">Case Study: [Country name]</h2>
-    <!-- /wp:heading -->
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:heading -->
+	<h2 class="wp-block-heading">Case Study: [Country name]</h2>
+	<!-- /wp:heading -->
 
-    <!-- wp:paragraph -->
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec quam et ante gravida molestie ut ac lorem. Suspendisse sed lorem faucibus, euismod dolor non, malesuada mauris. Praesent tincidunt blandit dolor, sed convallis urna sollicitudin et. </p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec quam et ante gravida molestie ut ac lorem. Suspendisse sed lorem faucibus, euismod dolor non, malesuada mauris. Praesent tincidunt blandit dolor, sed convallis urna sollicitudin et. </p>
+	<!-- /wp:paragraph -->
 
-    <!-- wp:paragraph -->
-    <p>Vestibulum iaculis ante sit amet augue interdum convallis. Vivamus molestie dignissim erat auctor venenatis. Nulla a tristique tortor. Integer scelerisque venenatis orci blandit porta. Morbi pellentesque leo id bibendum dapibus. Nullam vestibulum elementum elementum.</p>
-    <!-- /wp:paragraph -->
+	<!-- wp:paragraph -->
+	<p>Vestibulum iaculis ante sit amet augue interdum convallis. Vivamus molestie dignissim erat auctor venenatis. Nulla a tristique tortor. Integer scelerisque venenatis orci blandit porta. Morbi pellentesque leo id bibendum dapibus. Nullam vestibulum elementum elementum.</p>
+	<!-- /wp:paragraph -->
 
-    <!-- wp:paragraph -->
-    <p>Proin tincidunt accumsan nisi, eu viverra tellus mollis pharetra. Integer viverra aliquam tellus, sit amet euismod nulla auctor sit amet. </p>
-    <!-- /wp:paragraph --></div>
-    <!-- /wp:column --></div>
-    <!-- /wp:columns -->
-    <!-- /wp:amnesty-core/block-section -->
+	<!-- wp:paragraph -->
+	<p>Proin tincidunt accumsan nisi, eu viverra tellus mollis pharetra. Integer viverra aliquam tellus, sit amet euismod nulla auctor sit amet. </p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns -->
+	<!-- /wp:amnesty-core/block-section -->
