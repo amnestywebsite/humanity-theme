9c5fcadc1370f54857c8bdd33b1f12235cda62d6
PaulGent84 <paul.gent@bigbite.net>
2024-05-13 16:13:47 +0100

    Update social-share.php

--- wp-content/themes/humanity-theme/includes/block-patterns/social-share.php
+++ wp-content/themes/humanity-theme/includes/block-patterns/social-share.php
@@ -27,18 +27,6 @@ if ( ! function_exists( 'amnesty_register_block_patterns' ) ) {
 				'content'     => file_get_contents( __DIR__ . '/views/social-share.html' ),
 			]
 		);
-
-		register_block_pattern(
-			'amnesty/66-33-text-with-video',
-			[
-				/* translators: [admin] */
-				'title'       => __( '66/33 Text with Video', 'amnesty' ),
-				/* translators: [admin] */
-				'description' => __( 'Two column layout with text and video embed', 'amnesty' ),
-				'categories'  => [ 'amnesty-core' ],
-				'content'     => file_get_contents( __DIR__ . '/views/66-33-text-with-video.html' ),
-			]
-		);
 	}
 }
 
