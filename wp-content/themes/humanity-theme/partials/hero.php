e7f3cb79c30ae5501739288b0f5411db6638ebce
Jay McPartland <jay@bigbite.net>
2024-07-02 15:42:15 +0100

    Fix output of Hero / Header on posts / pages

--- wp-content/themes/humanity-theme/partials/hero.php
+++ wp-content/themes/humanity-theme/partials/hero.php
@@ -9,35 +9,13 @@ if ( is_tax() ) {
 	return;
 }
 
-
-$object_id = amnesty_get_header_object_id();
-
-if ( ! amnesty_post_has_header( $object_id ) ) {
+if ( ! amnesty_post_has_hero() ) {
 	return;
 }
 
-$hero_show = false;
-$hero_data = amnesty_get_header_data( $object_id );
+$hero_data = amnesty_get_hero_data();
 
-$hero_data['attrs'] = wp_parse_args(
-	$hero_data['attrs'],
-	[
-		'imageID' => absint( get_post_meta( $object_id, '_thumbnail_id', true ) ),
-	],
-);
-
-$object = get_queried_object();
-if ( ! is_singular( [ 'post' ] ) && ! is_search() && ! is_404() ) {
-	if ( is_archive() && is_object( $object ) ) {
-		$hero_data['attrs']['title']   = $object->label;
-		$hero_data['attrs']['content'] = $object->labels->archives ?? $object->description;
-	}
-}
-
-$hero_show = 0 !== $hero_data['attrs']['imageID'];
-
-if ( $hero_show ) {
-	// phpcs:ignore
-	echo \Amnesty\Blocks\amnesty_render_header_block( $hero_data['attrs'], $hero_data['content'] );
-	amnesty_remove_header_from_content();
+if ( $hero_data ) {
+	echo wp_kses_post( render_hero_block( $hero_data['attrs'], $hero_data['content'], $hero_data['name'] ) );
+	add_filter( 'the_content', 'amnesty_remove_first_hero_from_content', 0 );
 }
