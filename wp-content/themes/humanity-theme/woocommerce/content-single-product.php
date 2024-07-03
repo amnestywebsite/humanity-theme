90d17be44948ee63d3537c16568ef732301c1ac1
Jay McPartland <jay@bigbite.net>
2024-07-03 15:59:29 +0100

    Ensure required WC hooks are hooked on product single

--- wp-content/themes/humanity-theme/woocommerce/content-single-product.php
+++ wp-content/themes/humanity-theme/woocommerce/content-single-product.php
@@ -19,6 +19,8 @@ defined( 'ABSPATH' ) || exit;
 
 global $product;
 
+amnesty_wc_add_single_product_hooks();
+
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
