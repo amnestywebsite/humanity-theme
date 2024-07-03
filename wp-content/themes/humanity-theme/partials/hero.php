d7593185e889104795fc95cb9304076217b916c2
Jay McPartland <jay@bigbite.net>
2024-07-03 13:11:11 +0100

    Filter out blank Hero data before render

--- wp-content/themes/humanity-theme/partials/hero.php
+++ wp-content/themes/humanity-theme/partials/hero.php
@@ -15,7 +15,7 @@ if ( ! amnesty_post_has_hero() ) {
 
 $hero_data = amnesty_get_hero_data();
 
-if ( $hero_data ) {
+if ( array_filter( $hero_data ) ) {
 	echo wp_kses_post( render_hero_block( $hero_data['attrs'], $hero_data['content'], $hero_data['name'] ) );
 	add_filter( 'the_content', 'amnesty_remove_first_hero_from_content', 0 );
 }
