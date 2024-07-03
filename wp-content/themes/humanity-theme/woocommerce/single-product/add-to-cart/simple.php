27a8cec6ab8cffd944b8872444cf13da7777dfbf
Jay McPartland <jay@bigbite.net>
2024-07-03 14:23:49 +0100

    Update single product WC template parts

--- wp-content/themes/humanity-theme/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/humanity-theme/woocommerce/single-product/add-to-cart/simple.php
@@ -10,9 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 7.0.1
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -24,17 +24,17 @@ if ( ! $product->is_purchasable() ) {
 }
 
 // phpcs:ignore
-echo wc_get_stock_html( $product );
+echo wc_get_stock_html( $product ); // WPCS: XSS ok.
 
 if ( ! $product->is_in_stock() ) {
 	return;
 }
 
+do_action( 'woocommerce_before_add_to_cart_form' );
+
 $quantity = filter_input( INPUT_POST, 'quantity', FILTER_SANITIZE_NUMBER_INT );
 
 ?>
-
-<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
 <form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
@@ -54,9 +54,11 @@ $quantity = filter_input( INPUT_POST, 'quantity', FILTER_SANITIZE_NUMBER_INT );
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<button class="single_add_to_cart_button btn btn--dark" type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 </form>
 
-<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+<?php
+
+do_action( 'woocommerce_after_add_to_cart_form' );
