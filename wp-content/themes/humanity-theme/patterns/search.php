435f8d8a313e28e29a05e9d139ab4dbdc9945398
Gentyspun <paul.gent@bigbite.net>
2024-09-06 14:57:31 +0100

    Adds a template for the regular search page before queries

--- wp-content/themes/humanity-theme/patterns/search.php
+++ wp-content/themes/humanity-theme/patterns/search.php
@@ -11,38 +11,26 @@ $location_slug = get_option( 'amnesty_location_slug' ) ?: 'location';
 
 ?>
 
-<!-- wp:group {"tagName":"div","className":"container search-container has-gutter"} -->
-<div class="wp-block-group container search-container has-gutter">
-	<!-- wp:amnesty-core/search-form /-->
+<!-- wp:query {"queryId":0,"query":{"perPage":12,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"className":"section--tinted"} -->
+<div class="wp-block-query section--tinted">
+	<!-- wp:post-template {"layout":{"type":"constrained","justifyContent":"left"}} -->
 
-	<!-- wp:group {"tagName":"div","className":"section search-results section--tinted"} -->
-	<div class="wp-block-group section search-results section--tinted">
-		<!-- wp:amnesty-core/archive-header /-->
-
-		<!-- wp:query {"inherit":true} -->
-		<div class="wp-block-query">
-		<!-- wp:post-template {"layout":{"type":"constrained","justifyContent":"left"}} -->
-
-			<!-- wp:group {"tagName":"article","className":"post post--result"} -->
-			<article class="wp-block-group post post--result">
-				<!-- wp:post-terms {"term":"category","className":"post-category"} /-->
-				<!-- wp:post-terms {"term":"<?php echo esc_attr( $location_slug ); ?>","className":"post-location"} /-->
-				<!-- wp:post-terms {"term":"post_tag","className":"post-topic"} /-->
-				<!-- wp:post-title {"isLink":true,"className":"post-title"} /-->
-				<!-- wp:post-excerpt {"className":"post-excerpt"} /-->
-				<!-- wp:post-date {"className":"post-byline"} /-->
-			</article>
-			<!-- /wp:group -->
-
-		<!-- /wp:post-template -->
-		</div>
-		<!-- /wp:query -->
-		<!-- wp:query-pagination {"align":"center","className":"section section--small","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
-			<!-- wp:query-pagination-previous /-->
-			<!-- wp:query-pagination-numbers /-->
-			<!-- wp:query-pagination-next /-->
-		<!-- /wp:query-pagination -->
-	</div>
+	<!-- wp:group {"tagName":"article","className":"post post--result"} -->
+	<article class="wp-block-group post post--result">
+		<!-- wp:post-terms {"term":"category","className":"post-category"} /-->
+		<!-- wp:post-terms {"term":"<?php echo esc_attr( $location_slug ); ?>","className":"post-location"} /-->
+		<!-- wp:post-terms {"term":"topic","className":"post-topic"} /-->
+		<!-- wp:post-title {"isLink":true,"className":"post-title"} /-->
+		<!-- wp:post-excerpt {"className":"post-excerpt"} /-->
+		<!-- wp:post-date {"className":"post-byline"} /-->
+	</article>
 	<!-- /wp:group -->
+
+	<!-- /wp:post-template -->
+	<!-- wp:query-pagination {"align":"center","className":"section section--small post-paginationContainer","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
+	<!-- wp:query-pagination-previous {"className":"post-paginationLink post-paginationPrevious"} /-->
+	<!-- wp:query-pagination-numbers {"className":"page-numbers"} /-->
+	<!-- wp:query-pagination-next {"className":"post-paginationLink post-paginationNext"} /-->
+	<!-- /wp:query-pagination -->
 </div>
-<!-- /wp:group -->
+<!-- /wp:query -->
