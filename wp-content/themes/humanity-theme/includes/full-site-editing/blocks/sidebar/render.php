d172232e5bb86775eb0d79e449894d18576086f5
Jay McPartland <jay@bigbite.net>
2024-10-04 16:33:17 +0100

    Fixup sidebar renderer
    Account for output differences between pages and posts

--- wp-content/themes/humanity-theme/includes/full-site-editing/blocks/sidebar/render.php
+++ wp-content/themes/humanity-theme/includes/full-site-editing/blocks/sidebar/render.php
@@ -2,6 +2,29 @@
 
 declare( strict_types = 1 );
 
+if ( ! function_exists( 'amnesty_get_sidebar_id' ) ) {
+	/**
+	 * Get the correct sidbar ID for the current object
+	 *
+	 * @return int
+	 */
+	function amnesty_get_sidebar_id(): int {
+		// post-level override
+		$sidebar_id = absint( get_post_meta( get_the_ID(), '_sidebar_id', true ) );
+
+		if ( $sidebar_id ) {
+			return $sidebar_id;
+		}
+
+		// global default
+		return match ( get_post_type() ) {
+			'post'  => absint( amnesty_get_option( '_default_sidebar' )[0] ?? 0 ),
+			'page'  => absint( amnesty_get_option( '_default_sidebar_page' )[0] ?? 0 ),
+			default => 0,
+		};
+	}
+}
+
 if ( ! function_exists( 'render_sidebar_block' ) ) {
 	/**
 	 * Render the sidebar block
@@ -9,10 +32,8 @@ if ( ! function_exists( 'render_sidebar_block' ) ) {
 	 * @return string
 	 */
 	function render_sidebar_block(): string {
-		$current = get_the_ID() ?: 0;
-
 		$content_maximised = amnesty_validate_boolish(
-			get_post_meta( $current, '_maximise_post_content', true ),
+			get_post_meta( get_the_ID(), '_maximise_post_content', true ),
 			false,
 		);
 
@@ -21,35 +42,24 @@ if ( ! function_exists( 'render_sidebar_block' ) ) {
 		}
 
 		$sidebar_disabled = amnesty_validate_boolish(
-			get_post_meta( $current, '_disable_sidebar', true ),
+			get_post_meta( get_the_ID(), '_disable_sidebar', true ),
 			false,
 		);
 
-		if ( $sidebar_disabled ) {
+		if ( ! $sidebar_disabled ) {
+			$sidebar = get_post( amnesty_get_sidebar_id() );
+
+			return sprintf(
+				'<aside class="wp-block-group article-sidebar">%s</aside>',
+				apply_filters( 'the_content', $sidebar->post_content ?? '' ),
+			);
+		}
+
+		if ( ! is_page() ) {
 			// empty element is intentional
 			return '<aside class="wp-block-group article-sidebar"></aside>';
 		}
 
-		$sidebar_id = absint( get_post_meta( $current, '_sidebar_id', true ) );
-		if ( ! $sidebar_id ) {
-			$sidebar_id = absint( amnesty_get_option( '_default_sidebar' )[0] ?? 0 );
-		}
-
-		if ( ! $sidebar_id ) {
-			// empty element is intentional
-			return '<aside class="wp-block-group article-sidebar"></aside>';
-		}
-
-		$sidebar = get_post( $sidebar_id );
-
-		if ( ! $sidebar ) {
-			// empty element is intentional
-			return '<aside class="wp-block-group article-sidebar"></aside>';
-		}
-
-		return sprintf(
-			'<aside class="wp-block-group article-sidebar">%s</aside>',
-			apply_filters( 'the_content', $sidebar->post_content ),
-		);
+		return '';
 	}
 }
