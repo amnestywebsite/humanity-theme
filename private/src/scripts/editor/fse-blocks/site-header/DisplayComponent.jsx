a298df015983bbf2c7e1c5f030ec8c0712e58504
Jay McPartland <jay@bigbite.net>
2024-10-08 15:22:56 +0100

    Remove site header block and template part
    We need the components to render above
    the 'wp-site-blocks' container, as that
    container isn't a scroll container.
    The sticky header won't stick correctly
    if its parent isn't a scroll container.

--- private/src/scripts/editor/fse-blocks/site-header/DisplayComponent.jsx
+++
@@ -1,7 +0,0 @@
-const { serverSideRender: ServerSideRender } = wp;
-
-const SiteHeader = () => (
-  <ServerSideRender block="amnesty-core/site-header" className="site-header" />
-);
-
-export default SiteHeader;
