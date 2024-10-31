7b88390687fa8f8784a40625c33d4b7a2e9f5163
Gentyspun <paul.gent@bigbite.net>
2024-10-31 14:48:55 +0000

    Enqueues a bunch of custom block styles so they use the new API version
    Removed the old version, these include:
    - action
    - background media
    - blockquote
    - call to action
    - collapsable
    - countdown timer
    - custom card

--- source/blocks/download/render.php
+++ source/blocks/download/render.php
@@ -7,10 +7,18 @@ if ( ! count( $attributes['files'] ) ) {
 
 $attributes = apply_filters( 'amnesty_download_block_attributes', $attributes );
 
+echo '<pre>';
+var_dump( $attributes );
+echo '</pre>';
+
 $files  = $attributes['files'];
 $text   = $attributes['buttonText'];
 $colour = sprintf( 'btn--%s', $attributes['style'] );
 $align  = sprintf( 'align%s', $attributes['alignment'] );
+
+echo '<pre>';
+var_dump(count( $files ));
+echo '</pre>';
 
 if ( count( $files ) < 2 ) :
 	if ( empty( $files[0]['id'] ) ) {
@@ -42,33 +50,39 @@ if ( count( $files ) < 2 ) :
 
 endif;
 
-$first_url  = $files[0]['link'] ?? wp_get_attachment_url( $files[0]['id'] );
-$first_name = basename( $files[0]['link'] ?? wp_get_attachment_url( $files[0]['id'] ) );
-$options    = [];
+if ( count( $files ) < 2 ) :
 
-foreach ( $files as $file ) {
-	$key = $file['link'] ?? wp_get_attachment_url( $file['id'] );
+	$first_url  = $files[0]['link'] ?? wp_get_attachment_url( $files[0]['id'] );
+	$first_name = basename( $files[0]['link'] ?? wp_get_attachment_url( $files[0]['id'] ) );
+	$options    = [];
 
-	$options[ $key ] = $file['title'];
-}
+	foreach ( $files as $file ) {
+		$key = $file['link'] ?? wp_get_attachment_url( $file['id'] );
 
-?>
-<div class="<?php echo esc_attr( classnames( 'download-block is-multiple', $align, $attrs['className'] ?? null ) ); ?>">
-	<?php
-
-	amnesty_render_custom_select(
-		[
-			'label'      => wp_trim_words( $files[0]['title'], 8 ),
-			'is_control' => true,
-			'options'    => $options,
-		]
-	);
+		$options[ $key ] = $file['title'];
+	}
 
 	?>
-	<a
-		class="btn btn--download <?php echo esc_attr( $colour ); ?>"
-		href="<?php echo esc_url( $first_url ); ?>"
-		download="<?php echo esc_attr( $first_name ); ?>"
-		role="button"
-	><?php echo esc_html( $text ); ?></a>
-</div>
+	<div class="<?php echo esc_attr( classnames( 'download-block is-multiple', $align, $attrs['className'] ?? null ) ); ?>">
+		<?php
+
+		amnesty_render_custom_select(
+			[
+				'label'      => wp_trim_words( $files[0]['title'], 8 ),
+				'is_control' => true,
+				'options'    => $options,
+			]
+		);
+
+		?>
+		<a
+			class="btn btn--download <?php echo esc_attr( $colour ); ?>"
+			href="<?php echo esc_url( $first_url ); ?>"
+			download="<?php echo esc_attr( $first_name ); ?>"
+			role="button"
+		><?php echo esc_html( $text ); ?></a>
+	</div>
+
+	<?php
+
+endif;
