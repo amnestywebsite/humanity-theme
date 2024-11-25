2a141a2d11f41354ed6a9357fadd652b273d94cf
Gentyspun <paul.gent@bigbite.net>
2024-11-25 16:40:38 +0000

    Updates the old banner block and button block to API V3 and hides from them from the inserter since they are deprecated

--- source/blocks-deprecated/button/index.js
+++ source/blocks-deprecated/button/index.js
@@ -9,19 +9,5 @@ import { registerBlockType } from '@wordpress/blocks';
 registerBlockType(metadata, {
   ...metadata,
   edit,
-  save: ({ attributes }) => {
-    const { style = false, ctaLink, ctaText } = attributes;
-
-    if (isEmpty(ctaLink) || isEmpty(ctaText)) {
-      return null;
-    }
-
-    const linkClasses = classnames('btn', { [`btn--${style}`]: style });
-
-    return (
-      <a href={ctaLink} className={linkClasses}>
-        <RichText.Content tagName="span" value={ctaText} />
-      </a>
-    );
-  },
+  save: () => null,
 });
