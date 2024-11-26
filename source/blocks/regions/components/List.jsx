f40a30008a52c487b0246ad4e0a809e3c954b27b
Jay McPartland <jay@bigbite.net>
2024-11-26 13:03:51 +0000

    Fixup regions block API calls

--- source/blocks/regions/components/List.jsx
+++ source/blocks/regions/components/List.jsx
@@ -1,6 +1,6 @@
 const List = ({ terms = [], depth = 0, maxDepth }) => (
   <ul className={depth === 0 ? 'listItems' : 'children'}>
-    {Array.from(terms).map((term) => (
+    {terms && Array.from(terms).map((term) => (
       <li key={term.id} className={term.children ? 'has-children' : null}>
         <span>{term.name}</span>
         {depth < maxDepth && <List terms={term.children} depth={depth + 1} maxDepth={maxDepth} />}
