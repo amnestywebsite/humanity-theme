a945f83a2383f2b8aa40e54c69f3ad96b458153b
Gentyspun <paul.gent@bigbite.net>
2024-11-29 16:57:11 +0000

    Updated term list edit

--- source/blocks/term-list/edit.js
+++ source/blocks/term-list/edit.js
@@ -1,89 +1,49 @@
 import { useState, useEffect, Fragment } from 'react';
-import { filter, has, head, map } from 'lodash';
-import { BlockAlignmentToolbar, BlockControls, InspectorControls, RichText } from '@wordpress/block-editor';
+import { filter, map } from 'lodash';
+import { BlockAlignmentToolbar, BlockControls, InspectorControls, RichText, useBlockProps } from '@wordpress/block-editor';
 import { PanelBody, SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { addQueryArgs } from '@wordpress/url';
+import { useEntityRecords } from '@wordpress/core-data';
 
 const groupTerms = (terms) => {
-  const grouped = {};
-
-  terms
-    .filter(({ hidden, type }) => type === 'default' && !hidden)
-    .forEach((term) => {
-      const char = term.name.charAt(0).toUpperCase();
-      if (!has(grouped, char)) {
-        grouped[char] = [];
-      }
-      grouped[char].push(term);
-    });
-
-  return Object.keys(grouped)
-    .sort()
-    .reduce((object, key) => {
-      // eslint-disable-next-line no-param-reassign
-      object[key] = grouped[key];
-      return object;
-    }, {});
+  return terms?.reduce((grouped, term) => {
+    const char = term.name.charAt(0).toUpperCase();
+    if (!grouped[char]) grouped[char] = [];
+    grouped[char].push(term);
+    return grouped;
+  }, {}) || {};
 };
 
 const Edit = ({ attributes, className, setAttributes }) => {
-  const [taxonomies, setTaxonomies] = useState([]);
-  const [options, setOptions] = useState([]);
-  const [current, setCurrent] = useState({});
-  const [terms, setTerms] = useState([]);
-  const [cache, setCache] = useState({});
   const [activeLetter, setActiveLetter] = useState('');
 
-  useEffect(() => {
-    const { taxonomy } = attributes;
+  const { records: taxonomies, isResolving: taxonomiesLoading } = useEntityRecords('root', 'taxonomy');
+  const { records: terms, isResolving: termsLoading } = useEntityRecords('taxonomy', attributes.taxonomy, { per_page: -1 });
 
-    wp.apiFetch({ path: '/wp/v2/taxonomies/' }).then((rawTaxonomies) => {
-      const filteredTaxonomies = filter(rawTaxonomies, (t) => t.amnesty);
-      const selectedTaxonomy = head(filter(filteredTaxonomies, (t) => t.slug === taxonomy));
-      const taxonomyOptions = map(filteredTaxonomies, (tax) => ({ label: tax.name, value: tax.slug }));
-
-      setTaxonomies(filteredTaxonomies);
-      setCurrent(selectedTaxonomy);
-      setOptions(taxonomyOptions);
-    });
-  }, [attributes.taxonomy]);
+  const [groupedTerms, setGroupedTerms] = useState({});
+  const [options, setOptions] = useState([]);
 
   useEffect(() => {
-    if (current) {
-      fetchTerms();
+    if (!taxonomiesLoading) {
+      const filteredTaxonomies = filter(taxonomies, (t) => t.amnesty);
+      setOptions(map(filteredTaxonomies, (tax) => ({ label: tax.name, value: tax.slug })));
+      const currentTaxonomy = filteredTaxonomies.find((t) => t.slug === attributes.taxonomy);
+      if (currentTaxonomy) {
+        setAttributes({ taxonomy: currentTaxonomy.slug });
+      }
     }
-  }, [current]);
+  }, [taxonomies, taxonomiesLoading, attributes.taxonomy, setAttributes]);
 
-  const fetchTerms = () => {
-    const { rest_base } = current;
-
-    const path = addQueryArgs(`/wp/v2/${rest_base}/`, {
-      hide_empty: 'false',
-      per_page: '250',
-    });
-
-    if (cache[path]) {
-      setTerms(cache[path]);
-      return;
+  useEffect(() => {
+    if (!termsLoading && terms) {
+      const grouped = groupTerms(terms);
+      setGroupedTerms(grouped);
+      setActiveLetter(Object.keys(grouped)[0] || '');
     }
-
-    wp.apiFetch({ path }).then((data) => {
-      const groupedTerms = groupTerms(data);
-
-      setTerms(groupedTerms);
-      setActiveLetter(Object.keys(groupedTerms)[0]);
-      setCache((prevCache) => ({
-        ...prevCache,
-        [path]: groupedTerms,
-      }));
-    });
-  };
+  }, [terms, termsLoading]);
 
   const handleSetActiveLetter = (letter) => {
-    if (terms[letter]) {
-      setActiveLetter(letter);
-    }
+    if (groupedTerms[letter]) setActiveLetter(letter);
   };
 
   const renderInspectorControls = () => (
@@ -108,29 +68,34 @@ const Edit = ({ attributes, className, setAttributes }) => {
     </BlockControls>
   );
 
-  const letterItems = Array.from(terms[activeLetter] || []);
+  const letterItems = groupedTerms[activeLetter] || [];
+
+  const blockProps = useBlockProps({
+    className,
+  });
 
   return (
     <Fragment>
       {renderInspectorControls()}
       {renderBlockControls()}
-      <aside className={className}>
+      <aside {...blockProps}>
         <RichText
           tagName="h2"
           format="string"
-          className={attributes.alignment ? `is-${attributes.alignment}-aligned` : null}
+          className={attributes.alignment ? `is-${attributes.alignment}-aligned` : ''}
           value={attributes.title}
           onChange={(title) => setAttributes({ title })}
           placeholder={__('A-Z of Countries and Regions', 'amnesty')}
           withoutInteractiveFormatting
         />
         <div className="navigation">
-          {Object.keys(terms).map((letter) => (
+          {/* Render buttons for each letter */}
+          {Object.keys(groupedTerms).map((letter) => (
             <button
               key={letter}
-              className={letter === activeLetter ? 'is-active' : null}
+              className={letter === activeLetter ? 'is-active' : ''}
               onClick={() => handleSetActiveLetter(letter)}
-              disabled={(terms[letter] || []).length === 0}
+              disabled={groupedTerms[letter]?.length === 0}
             >
               {letter}
             </button>
