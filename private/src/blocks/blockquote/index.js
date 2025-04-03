28d3fb8f0b36f1b2ddf531c8c92062d9241bdd27
Jay McPartland <jay@bigbite.net>
2025-04-03 11:43:21 +0100

    Fix quote block styles

--- private/src/blocks/blockquote/index.js
+++ private/src/blocks/blockquote/index.js
@@ -1,9 +1,7 @@
 import './style.scss';
 import './editor.scss';
 
-import { assign } from 'lodash';
 import { registerBlockType } from '@wordpress/blocks';
-import { addFilter } from '@wordpress/hooks';
 
 import edit from './edit.jsx';
 import metadata from './block.json';
@@ -16,19 +14,4 @@ registerBlockType(metadata, {
   transforms,
   edit,
   save: () => null,
-});
-
-/**
- * Hide the core blockquote from the list, as we have our own version.
- */
-addFilter('blocks.registerBlockType', 'amnesty-core', (settings, name) => {
-  if (name === 'core/quote') {
-    return assign({}, settings, {
-      supports: assign({}, settings.supports, {
-        inserter: false,
-      }),
-    });
-  }
-
-  return settings;
 });
