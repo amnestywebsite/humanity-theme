891b4608658c61524ce0e45440b7abef3eb943ca
Jay McPartland <jay@bigbite.net>
2025-04-23 13:01:03 +0100

    Fix incorrect sprite path

--- private/src/register-frontend-assets.php
+++ private/src/register-frontend-assets.php
@@ -21,7 +21,7 @@ if ( ! function_exists( 'humanity_register_frontend_assets' ) ) {
 
 		wp_enqueue_script( 'amnesty-theme', get_template_directory_uri() . '/build/frontend/index.js', $deps, $theme->get( 'Version' ), true );
 		wp_enqueue_style( 'amnesty-theme', get_template_directory_uri() . '/build/frontend/index.css', [], $theme->get( 'Version' ) );
-		wp_add_inline_style( 'amnesty-theme', sprintf( ':root{--amnesty-icon-path:url("%s"),none}', esc_url( get_template_directory_uri() . '/assets/images/sprite.svg' ) ) );
+		wp_add_inline_style( 'amnesty-theme', sprintf( ':root{--amnesty-icon-path:url("%s"),none}', esc_url( get_template_directory_uri() . '/images/sprite.svg' ) ) );
 
 		$ol_characters = amnesty_get_option( 'ol_locale_option', 'amnesty_localisation_options_page' );
 
