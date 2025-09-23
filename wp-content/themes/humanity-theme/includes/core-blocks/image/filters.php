b2807e433c5117b292699f67a3f73b4df83cdf8a
Jay McPartland <jay@bigbite.net>
2025-09-23 14:25:04 +0100

    Only ignore images for credit when they have it

--- wp-content/themes/humanity-theme/includes/core-blocks/image/filters.php
+++ wp-content/themes/humanity-theme/includes/core-blocks/image/filters.php
@@ -8,15 +8,14 @@ if ( ! function_exists( 'amnesty_build_new_image_block_tag' ) ) {
 	/**
 	 * Check whether an image block should have image metadata added
 	 *
-	 * @param array $block     the parsed block
-	 * @param array $image_tag img tag regex result
-	 * @param bool  $wrap      whether to wrap the output
+	 * @param array          $block     the parsed block
+	 * @param array          $image_tag img tag regex result
+	 * @param Get_Image_Data $image_obj object representing the image
+	 * @param bool           $wrap      whether to wrap the output
 	 *
 	 * @return string
 	 */
-	function amnesty_build_new_image_block_tag( array $block, array $image_tag, bool $wrap = true ): string {
-		$image_object = new Get_Image_Data( $block['attrs']['id'] );
-
+	function amnesty_build_new_image_block_tag( array $block, array $image_tag, Get_Image_Data $image_obj, bool $wrap = true ): string {
 		$new_image_tag = match ( str_contains( $image_tag[0], 'class' ) ) {
 			true  => str_replace( 'class="', 'class="aiic-ignore ', $image_tag[0] ),
 			false => str_replace( '<img ', '<img class="aiic-ignore" ', $image_tag[0] ),
@@ -29,7 +28,7 @@ if ( ! function_exists( 'amnesty_build_new_image_block_tag' ) ) {
 			$hide_caption = '1' === get_post_meta( get_the_ID(), '_hide_featured_image_caption', true );
 		}
 
-		$new_image_tag .= $image_object->metadata( ! $hide_caption, ! $hide_copyright );
+		$new_image_tag .= $image_obj->metadata( ! $hide_caption, ! $hide_copyright );
 
 		if ( false === $wrap ) {
 			return $new_image_tag;
@@ -63,11 +62,16 @@ if ( ! function_exists( 'amnesty_add_image_metadata_to_image_block' ) ) {
 			return $content;
 		}
 
-		$should_not_wrap = ! str_contains( $block['attrs']['className'] ?? '', 'article-figure' );
-		$new_opening_tag = str_replace( 'class="', 'class="aimc-ignore ', $opening_tag );
-		$new_image_tag   = amnesty_build_new_image_block_tag( $block, $image_tag, wrap: $should_not_wrap );
+		$image_object = new Get_Image_Data( $block['attrs']['id'] );
 
-		$content = str_replace( $opening_tag, $new_opening_tag, $content );
+		if ( $image_object->credit() ) {
+			$new_opening_tag = str_replace( 'class="', 'class="aimc-ignore ', $opening_tag );
+			$content         = str_replace( $opening_tag, $new_opening_tag, $content );
+		}
+
+		$should_not_wrap = ! str_contains( $block['attrs']['className'] ?? '', 'article-figure' );
+		$new_image_tag   = amnesty_build_new_image_block_tag( $block, $image_tag, $image_object, wrap: $should_not_wrap );
+
 		$content = str_replace( $image_tag[0], $new_image_tag, $content );
 
 		return $content;
