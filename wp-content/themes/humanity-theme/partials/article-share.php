0fc7f20ffcda9d02b59711899c62e388b01c6305
Jay McPartland <jay@bigbite.net>
2025-09-24 09:55:36 +0100

    Implement theme fixes to support media copyright plugin

--- wp-content/themes/humanity-theme/partials/article-share.php
+++ wp-content/themes/humanity-theme/partials/article-share.php
@@ -9,7 +9,7 @@
 spaceless();
 
 ?>
-<ul class="article-share" role="complementary" aria-label="<?php /* translators: [front] https://wordpresstheme.amnesty.org/the-theme/global-elements/m004-social-share/ Accessibility text */ esc_attr_e( 'Social sharing options', 'amnesty' ); ?>">
+<ul class="aimc-ignore article-share" role="complementary" aria-label="<?php /* translators: [front] https://wordpresstheme.amnesty.org/the-theme/global-elements/m004-social-share/ Accessibility text */ esc_attr_e( 'Social sharing options', 'amnesty' ); ?>">
 	<li>
 		<a class="article-shareFacebook" target="_blank" rel="noreferrer noopener" href="https://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" title="<?php /* translators: [front] https://wordpresstheme.amnesty.org/the-theme/global-elements/m004-social-share/ text shows on hover  */ esc_attr_e( 'Share on Facebook', 'amnesty' ); ?>">
 			<img src="<?php echo esc_url( amnesty_asset_uri( 'images' ) ); ?>/icon-facebook.svg" alt="<?php /* translators: [front] https://wordpresstheme.amnesty.org/the-theme/global-elements/m004-social-share/ */ esc_attr_e( 'Facebook Logo', 'amnesty' ); ?>">
