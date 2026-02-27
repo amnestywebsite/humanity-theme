c02f6860592910bbe7edc378b6124e3d77fc2d8f
Jay McPartland <jay.mcpartland@amnesty.org>
2026-02-27 10:50:53 +0000

    Add Index Number to metadata fields

--- private/src/scripts/editor/plugins/appearance-options/components/Metadata.jsx
+++ private/src/scripts/editor/plugins/appearance-options/components/Metadata.jsx
@@ -1,9 +1,34 @@
 import Byline from './Byline.jsx';
 import ShareButtons from './ShareButtons.jsx';
 
-const { PanelBody, ToggleControl } = wp.components;
+const { useCallback } = React;
+const { PanelBody, TextControl, ToggleControl } = wp.components;
 const { compose, ifCondition } = wp.compose;
+const { useEntityProp } = wp.coreData;
+const { useSelect } = wp.data;
+const { store: editorStore } = wp.editor;
 const { __ } = wp.i18n;
+
+function IndexNumber() {
+  const postId = useSelect((select) => select(editorStore).getCurrentPostId(), []);
+  const postType = useSelect((select) => select(editorStore).getCurrentPostType(), []);
+  const [meta, setMeta] = useEntityProp('postType', postType, 'meta', postId);
+
+  const editIndexNumber = useCallback(
+    (indexNumber) => {
+      setMeta({ ...meta, amnesty_index_number: indexNumber });
+    },
+    [meta, setMeta],
+  );
+
+  return (
+    <TextControl
+      label={__('Index Number', 'amnesty')}
+      value={meta?.amnesty_index_number ?? ''}
+      onChange={editIndexNumber}
+    />
+  );
+}
 
 /**
  * Render an option to toggle the published date's visibility
@@ -69,6 +94,8 @@ const UpdatedDate = ({ createMetaUpdate, props }) => (
  */
 const Metadata = ({ createMetaUpdate, props }) => (
   <PanelBody title={/* translators: [admin] */ __('Metadata', 'amnesty')} initialOpen={false}>
+    <IndexNumber />
+    <div style={{ height: '24px' }} />
     <PublishedDate createMetaUpdate={createMetaUpdate} props={props} />
     <UpdatedDate createMetaUpdate={createMetaUpdate} props={props} />
     <ShareButtons createMetaUpdate={createMetaUpdate} props={props} />
